fef887029b0872fb049150a765925279c067bf1b
codewalker <codewalker@hardkernel.com>
2014-05-15 09:44:17 +0900

    samsung patch

--- VolumeManager.h
+++ VolumeManager.h
@@ -25,6 +25,10 @@
 
 #include "Volume.h"
 
+#if defined(BOARD_USES_HDMI)
+#include "SecHdmiClient.h"
+#endif
+
 /* The length of an MD5 hash when encoded into ASCII hex characters */
 #define MD5_ASCII_LENGTH_PLUS_NULL ((MD5_DIGEST_LENGTH*2)+1)
 
@@ -67,6 +71,10 @@ private:
     int                    mUmsDirtyRatio;
     int                    mVolManagerDisabled;
 
+#if defined(BOARD_USES_HDMI)
+    android::SecHdmiClient   *mHdmiClient;
+#endif
+
 public:
     virtual ~VolumeManager();
 
@@ -74,6 +82,10 @@ public:
     int stop();
 
     void handleBlockEvent(NetlinkEvent *evt);
+
+#if defined(BOARD_USES_HDMI)
+    void handleHdmiEvent(NetlinkEvent *evt);
+#endif
 
     int addVolume(Volume *v);
 
